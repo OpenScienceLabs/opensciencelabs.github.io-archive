11a6d0589f7eb25d7095dadc86780d6535e10a2e
EverVino <evervino00@gmail.com>
2022-05-03 20:04:08 -0400

    Corrigiendo algunos typos

--- conf.py
+++ conf.py
@@ -1125,7 +1125,7 @@ SHOW_SOURCELINK = False
 # Extra things you want in the pages HEAD tag. This will be added right
 # before </head>
 # (translatable)
-#EXTRA_HEAD_DATA = ""
+EXTRA_HEAD_DATA = ""
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
